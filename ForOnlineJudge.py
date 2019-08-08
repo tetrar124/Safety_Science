5a041cd6aa8c46d6d2a9b5c76877dbf44544885d
tetrar124 <36783730+tetrar124@users.noreply.github.com>
2019-08-08 19:00:04 +0900

    2019年 8月  8日 木曜日 19:00:04

---
+++ ForOnlineJudge.py
@@ -0,0 +1,14 @@
+
+def sosu(n):
+    i = 3
+    sosu = [3]
+    #素数候補生成
+    for i > 10000000000:
+        for j in sosu:
+            if i % j ==0:
+                sosu.append(j)
+            else:
+                pass
+
+
+
